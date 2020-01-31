b5363623045488d91e3336c274cafdf52e764fb8
henrikengelbrink <henrik@keeet.io>
2020-01-31 20:48:47 +0100

    feat: Enable remote Backend

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-# terraform {
-#   backend "remote" {
-#     hostname = "app.terraform.io"
-#     organization = "hengel2810"
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "hengel2810"
 
-#     workspaces {
-#       name = "clothing-scanner"
-#     }
-#   }
-# }
+    workspaces {
+      name = "se_09"
+    }
+  }
+}
 
 provider "digitalocean" {
   token = "${var.do_token}"
