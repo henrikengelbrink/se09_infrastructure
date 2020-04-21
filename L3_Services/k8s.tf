f6b4b63483c7f9914cecbcf5ce00174031de010c
henrikengelbrink <henrik@keeet.io>
2020-04-21 18:15:47 +0200

    feat: Add basic k8s pod security policy

--- L3_Services/k8s.tf
+++ L3_Services/k8s.tf
@@ -7,3 +7,9 @@ provider "kubernetes" {
   token                  = data.digitalocean_kubernetes_cluster.k8s_cluster.kube_config.0.token
   cluster_ca_certificate = base64decode(data.digitalocean_kubernetes_cluster.k8s_cluster.kube_config.0.cluster_ca_certificate)
 }
+
+resource "null_resource" "pod_security_policy" {
+  provisioner "local-exec" {
+    command = "kubectl --kubeconfig ../L1_CloudInfrastructure/kubeconfig.yaml --namespace=default apply -f ./crds/psd.yml"
+  }
+}
